b6eb214ebdd9ea6cb95f8c51b64d5f7b0c45ed12
Bekarys <bekzhanbekarys@gmail.com>
2024-08-08 15:04:43 +0500

    Added custom app icons and a SplashScreen

--- android/app/src/main/java/com/coffeeshop/MainActivity.kt
+++ android/app/src/main/java/com/coffeeshop/MainActivity.kt
@@ -6,6 +6,8 @@ import com.facebook.react.ReactActivityDelegate
 import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint.fabricEnabled
 import com.facebook.react.defaults.DefaultReactActivityDelegate
 
+import org.devio.rn.splashscreen.SplashScreen;
+
 class MainActivity : ReactActivity() {
 
   /**
@@ -15,7 +17,8 @@ class MainActivity : ReactActivity() {
   override fun getMainComponentName(): String = "CoffeeShop"
 
   override fun onCreate(savedInstanceState: Bundle?) {
-    super.onCreate(null)
+    SplashScreen.show(this)
+    super.onCreate(savedInstanceState)
   }
   /**
    * Returns the instance of the [ReactActivityDelegate]. We use [DefaultReactActivityDelegate]
